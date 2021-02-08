b17fba1447dacb43974ae9bae79d011f79a03ecf
Chris Perry <chris@cwperry.dev>
2021-02-08 14:51:08 -0500

    handle parsing pipe delimited rows into records

--- src/person_data_processor/core.clj
+++ src/person_data_processor/core.clj
@@ -1,5 +1,17 @@
 (ns person-data-processor.core
+  (:require [clojure.string :as s]
+            [java-time :as jt])
   (:gen-class))
+
+(defn input-row->record [row]
+  (let [[last-name first-name email favorite-color date-of-birth] (->> (s/split row #"\|")
+                                                                       (map s/trim)
+                                                                       (into []))]
+    {:last-name      last-name
+     :first-name     first-name
+     :email          email
+     :favorite-color favorite-color
+     :date-of-birth  (jt/local-date "MM/dd/yyyy" date-of-birth)}))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/person_data_processor/core_test.clj
+++ test/person_data_processor/core_test.clj
@@ -1,7 +1,13 @@
 (ns person-data-processor.core-test
   (:require [clojure.test :refer :all]
-            [person-data-processor.core :refer :all]))
+            [person-data-processor.core :refer :all]
+            [java-time :as jt]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-input-row->record
+  (testing "create a record from a pipe delimited row"
+    (is (= {:last-name      "Example"
+            :first-name     "Chris"
+            :email          "chris@example.com"
+            :favorite-color "blue"
+            :date-of-birth  (jt/local-date 1992 05 13)}
+           (input-row->record "Example | Chris | chris@example.com | blue | 05/13/1992")))))
