568f7498ddf2b7561c889469cf007c7f7e0c8df6
Chris Perry <chris@cwperry.dev>
2021-02-09 10:46:48 -0500

    ensure cli output formats date properly

--- src/person_data_processor/cli.clj
+++ src/person_data_processor/cli.clj
@@ -1,16 +1,19 @@
 (ns person-data-processor.cli
   (:require [mount.core :as m]
             [person-data-processor.parse :as parse]
-            [person-data-processor.db :as db])
+            [person-data-processor.db :as db]
+            [java-time :as jt])
   (:gen-class))
 
+(defn- date->date-str [date]
+  (jt/format "MM/dd/yyyy" (jt/local-date date)))
 
 (defn -main
   [sort-by & args]
   (m/start)
   (db/create-records-table!)
-  (let [records (parse/input-files->records args)]
-    (doseq [record records]
-      (db/insert-record! record)))
-  (clojure.pprint/print-table (db/get-records (keyword sort-by)))
+  (doseq [record (parse/input-files->records args)]
+    (db/insert-record! record))
+  (clojure.pprint/print-table (->> (db/get-records (keyword sort-by))
+                                   (map #(update % :date_of_birth date->date-str))))
   (m/stop))
